898a5cdcb05dbb763f8ad5822e720069407e1911
jhelcy-sandoval <jhelcysandoval@gmail.com>
2025-11-06 23:00:11 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { ProyectosComponent } from '../../components/proyectos/proyectos.component';
 import { HabilidadesComponent } from '../../components/habilidades/habilidades.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProyectosComponent, HabilidadesComponent],
+  imports: [CommonModule, ProyectosComponent, HabilidadesComponent], 
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -13,15 +14,17 @@ export default class HomeComponent {
   @ViewChild('email') email!: ElementRef;
   isCopied = false;
   emailText = 'jhelcysandoval@gmail.com';
+  showAlert = false; 
 
   copyClipboard() {
     navigator.clipboard.writeText(this.emailText).then(() => {
       this.isCopied = true;
-      console.log('good');
+      this.showAlert = true; 
+
       setTimeout(() => {
         this.isCopied = false;
-      }, 2000); 
+        this.showAlert = false; 
+      }, 2000);
     });
   }
 }
- 
